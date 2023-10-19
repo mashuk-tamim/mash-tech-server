8b144c116501e8b0eeb2f5000e658d2d7d06270f
mashuk-tamim <tamimmashuk328@gmail.com>
2023-10-19 12:40:34 +0600

    server giving specific product based on id

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get("/products/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post("/products", async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
